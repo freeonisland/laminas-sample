bce235c66895430d6cc67cc2db3a33e8a920089c
freeonisland <freeonisland@yahoo.fr>
2020-04-24 16:26:01 +0200

    remove docker mac

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -19,4 +19,9 @@ class IndexController extends AbstractActionController
     {
         return new ViewModel();
     }
+
+    public function startAction()
+    {
+        return new ViewModel();
+    }
 }
